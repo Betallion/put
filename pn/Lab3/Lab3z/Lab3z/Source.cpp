efc00792ee9159e72a208e02834d7c922d83e373
Kacper Leporowski <kacperleporowski@gmail.com>
2022-03-31 13:30:02 +0200

    bst

---
+++ pn/Lab3/Lab3z/Lab3z/Source.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+	struct Student
+	{
+		char imie[15];
+		char nazwisko[30];
+		int indeks;
+		char kierunek[30];
+		int rok;
+		int grupa;
+	};
+
+	return 0;
+}
